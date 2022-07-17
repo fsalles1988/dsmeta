d79bd4c4d0a234e6e446c53043ac0cfbadca59ca
Felipe da Silva Salles <felipesalles1988@gmail.com>
2022-07-17 17:03:10 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/Notification-icon.svg'
+import icon from '../../assets/img/Notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
